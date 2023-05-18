fb55c83329c394f5fbef0eb46a33281e9d8e7a15
David Requeno <davidrequeno52@gmail.com>
2023-05-17 20:55:06 -0600

    string y concatenar

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <string>
 
 int main() {
     // Write C++ code here
+    //Numeros enteros
     int num = 56;
+
+    //Numeros decimales
     float numDecimal = 0.56;
     double numDecimalDouble = 1.74;
-    std::cout << "Hello world!";
+
+    //Verdadero o falso
+    bool TrueFalse = true;
+
+    //Almacena una sola letra
+    char miCaracter = 'D';
+
+    //Cadenas de texto
+    std::string cadena = "David";
+
+    //concatenar
+    std::cout << "Hello world!" << " " << cadena;
 
     return 0;
 }
