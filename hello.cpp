6faa5d8491c4802039724e55eff88e488f2a4e27
David Requeno <davidrequeno52@gmail.com>
2023-05-17 20:43:54 -0600

    variables c++

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    int num = 56;
+    float numDecimal = 0.56;
+    double numDecimalDouble = 1.74;
+    std::cout << "Hello world!";
+
+    return 0;
+}
