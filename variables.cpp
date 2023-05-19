0aa86fe2a94c0bec0572a56b720cede8b2940f6c
David Requeno <davidrequeno52@gmail.com>
2023-05-18 20:15:47 -0600

    Variables

---
+++ variables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Ejemplo de diferentes tipos de variables en C++
+
+    // Variable entera para almacenar números enteros
+    int numeroEntero = 56;
+
+    // Variable de punto flotante para almacenar números decimales
+    float numeroDecimal = 0.56;
+    double numeroDecimalDoble = 1.74;
+
+    // Variable booleana para almacenar verdadero o falso
+    bool verdaderoFalso = true;
+
+    // Variable de tipo carácter para almacenar una sola letra
+    char miCaracter = 'D';
+
+    // Variable de tipo cadena de texto
+    std::string miCadena = "David";
+
+    // Imprimir un saludo y concatenar una cadena de texto
+    std::cout << "¡Hola, mundo! Mi nombre es " << miCadena << std::endl;
+
+    return 0;
+}
