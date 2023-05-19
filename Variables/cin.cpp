c2ae42088f5e9b118ae84ff5abd7d26252d3b8d4
David Requeno <davidrequeno52@gmail.com>
2023-05-18 21:11:40 -0600

    cin c++

---
+++ Variables/cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int miVariable;
+
+    cout << "Ingrese variables:" << endl;
+    cin >> miVariable;
+
+    cout << "La variable guardada es: " << miVariable;
+    
+    return 0;
+}
