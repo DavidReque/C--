0d7bccbaab815b4b2144886c47489a394c385f3f
David Requeno <davidrequeno52@gmail.com>
2023-05-30 20:27:35 -0600

    Operadores matematicos

---
+++ Operadores/operadores.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+    // Operaciones básicas en C++
+
+    // Declaración e inicialización de variables
+    int numero1 = 17;
+    int numero2 = 5;
+
+    // Suma
+    int suma = numero1 + numero2;
+
+    // Resta
+    int resta = numero1 - numero2;
+
+    // Multiplicación
+    int multiplicacion = numero1 * numero2;
+
+    // División
+    int division = numero1 / numero2;
+
+    // Módulo (resto de la división)
+    int modulo = numero1 % numero2;
+
+    // Imprimir los resultados
+    std::cout << "Suma: " << suma << std::endl;
+    std::cout << "Resta: " << resta << std::endl;
+    std::cout << "Multiplicacion: " << multiplicacion << std::endl;
+    std::cout << "Division: " << division << std::endl;
+    std::cout << "Modulo: " << modulo << std::endl;
+
+    return 0;
+}
---
+++ Variables/inicializar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    int num2 = 0;
+    int num3 = 0;
+    int suma = 0;
+
+    int num1 = 5;
+
+    cin >> num2;
+
+    cout << num1;
+
+    cout << "La suma de las variables es: " << num1 + num2 << endl;
+
+    return 0;
+}
